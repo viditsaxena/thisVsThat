2707f3dacaf691f5ade92d355d99385fb6be4f79
Vidit Saxena <viditsax@gmail.com>
2015-11-16 13:49:51 -0500

    adds FAQ

--- client/js/init.js
+++ client/js/init.js
@@ -3,14 +3,9 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
+
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-
-$(document).ready(function(){
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal-trigger').leanModal();
-  $('.collapsible').collapsible({
-    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-  });
-});
